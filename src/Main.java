88bd0a01bb0c0bdb81bab3653b4cca52fba1e053
linke <linke@duck.com>
2025-05-12 17:02:18 +0300

    Complete Task 3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // task 1
 
-        System.out.println("Задача 1");
-
         int [] numbers = new int[]{1, 2, 3};
         double [] digits = {1.57, 7.654, 9.986};
         char [] chars = new char[15];
@@ -21,5 +19,21 @@ public class Main {
             System.out.print(digits[i] + ", ");
         }
         System.out.print("" + digits[digits.length - 1]);
+        System.out.println();
+
+        // task 3
+
+        System.out.println("Задача 3");
+
+        for (int i = numbers.length - 1; i > 0; i--) {
+            System.out.print(numbers[i] + ", ");
+        }
+        System.out.print(numbers[0]);
+        System.out.println();
+        for (int i = digits.length - 1; i > 0; i--) {
+            System.out.print(digits[i] + ", ");
+        }
+        System.out.print(digits[0]);
+        System.out.println();
     }
 }
