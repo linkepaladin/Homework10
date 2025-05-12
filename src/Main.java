6858de446ee5a57b381bdbf3c4610b440507b7ec
linke <linke@duck.com>
2025-05-12 16:03:24 +0300

    Complete Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        // task 1
+
+        System.out.println("Задача 1");
+
+        int [] numbers = new int[]{1, 2, 3};
+        double [] digits = {1.57, 7.654, 9.986};
+        char [] chars = new char[15];
     }
 }
